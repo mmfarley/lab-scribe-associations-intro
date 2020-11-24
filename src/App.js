bfbea79fe559afb52051451a3e75a3e9f8113595
Megan Farley <megan@molecule.io>
2020-11-24 13:40:08 -0600

    added properties to list(), updated map function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { tether } from '@triframe/designer'
 
 const UserDetails = tether(function* ({ Api }) {
     const { User } = Api
-    const [ user ] = yield User.list()
+    const [ user ] = yield User.list(`
+        name,
+        pets {
+            name
+        }
+    `)
     return (
         <div>
             <h1>{user.name}</h1>
             <ul>
-                {[].map(pet => (
+                {user.pets.map(pet => (
                     <li>
                         <span>{pet.name} </span>
                         <button onClick={() => pet.delete()}>Delete</button>
